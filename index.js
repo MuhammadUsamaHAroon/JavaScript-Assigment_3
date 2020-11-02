c5a8cb6c274c716b3032c5b32133c2a84a9c9a68
usama haroon <muhammadusamaharoon13579@gmail.com>
2020-11-02 22:28:05 +0500

    Nine Questions are done

---
+++ index.js
@@ -0,0 +1,72 @@
+// ARRAY
+// Question no.1
+
+var array=[]
+array.push("Bilal", "Izhan", "Shakeel", "Safdar", "Ahmed")
+console.log(array)
+
+// Question no.3
+
+var array=[]
+array.push("My Name is Usama")
+console.log(array)
+
+// Question no.4
+
+var array=[]
+array.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+console.log(array)
+
+// Question no.5
+
+var array=[]
+array.push(true, false)
+console.log(array)
+
+// Question no.6
+
+var array=[]
+array.push("Usama was born in ", 25, "November", 2001, "this is a", true, "line")
+console.log(array)
+
+// Question no.7
+
+var array=[]
+array.push("Ufone")
+array.push("Telenor")
+array.push("Zong")
+array.push("Warid")
+array.push("Jazz")
+console.log(array)
+
+// Question no.8
+
+var array=[]
+array.push("SSC")
+array.push("HSC")
+array.push("BCS")
+array.push("BS")
+array.push("BCOM")
+array.push("MS")
+array.push("M.Phil")
+array.push("PhD")
+console.log(array)
+
+// Question no.9
+
+var array=[]
+array.push("Avengers : Age of Ultron")
+array.push("Spectre")
+array.push("Jurassic World")
+array.push("Inside Out")
+console.log(array)
+
+// Question no.10
+
+var array=[]
+console.log(array)
+array[0]="Audi"
+array[5]="Lamborghini"
+console.log(array)
+
+// Question No.11
